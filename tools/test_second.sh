dda182f44efd4d24ee644ec181c7a60987b0d5ef
Daniel-YH-Cai <1155141377@link.cuhk.edu.hk>
2022-11-26 21:52:16 +0800

    Add test scripts

---
+++ tools/test_second.sh
@@ -0,0 +1,4 @@
+python3 test.py --cfg_file cfgs/kitti_models/second.yaml   \
+  --ckpt /home/nvidia/Desktop/motivation-experiment/det3dd/OpenPCDet/ckpt/second_7862.pth \
+  --workers 0 \
+  --batch_size 1
