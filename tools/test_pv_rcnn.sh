ae74fc725eeaf1b344f9002e69f3877b184c9352
Daniel-YH-Cai <1155141377@link.cuhk.edu.hk>
2022-11-26 21:26:19 +0800

    add batch size 1

--- tools/test_pv_rcnn.sh
+++ tools/test_pv_rcnn.sh
@@ -1,3 +1,4 @@
 python3 test.py --cfg_file cfgs/kitti_models/pv_rcnn.yaml   \
   --ckpt /home/nvidia/Desktop/motivation-experiment/det3dd/OpenPCDet/ckpt/pv_rcnn_8369.pth \
-  --workers 0
+  --workers 0 \
+  --batch_size 1
