e965fe2b9893b0a8be97d6ddcd62068948c2ca34
Daniel-YH-Cai <1155141377@link.cuhk.edu.hk>
2022-11-26 21:52:16 +0800

    Add test scripts

---
+++ tools/test_point_rcnn.sh
@@ -0,0 +1,4 @@
+python3 test.py --cfg_file cfgs/kitti_models/pointrcnn.yaml   \
+  --ckpt /home/nvidia/Desktop/motivation-experiment/det3dd/OpenPCDet/ckpt/pointrcnn_7870.pth \
+  --workers 0 \
+  --batch_size 1
---
+++ tools/test_pointpillar.sh
@@ -0,0 +1,4 @@
+python3 test.py --cfg_file cfgs/kitti_models/pointpillar.yaml   \
+  --ckpt /home/nvidia/Desktop/motivation-experiment/det3dd/OpenPCDet/ckpt/pointpillar_7728.pth \
+  --workers 0 \
+  --batch_size 1
