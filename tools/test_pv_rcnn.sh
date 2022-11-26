9ccb92f625b9b589b906f670e61759ff4007c8db
Daniel-YH-Cai <1155141377@link.cuhk.edu.hk>
2022-11-26 20:56:41 +0800

    Dataloader worker set to zero

--- tools/test_pv_rcnn.sh
+++ tools/test_pv_rcnn.sh
@@ -1,2 +1,3 @@
 python3 test.py --cfg_file cfgs/kitti_models/pv_rcnn.yaml   \
-  --ckpt /home/nvidia/Desktop/motivation-experiment/det3dd/OpenPCDet/ckpt/pv_rcnn_8369.pth
+  --ckpt /home/nvidia/Desktop/motivation-experiment/det3dd/OpenPCDet/ckpt/pv_rcnn_8369.pth \
+  --workers 0
