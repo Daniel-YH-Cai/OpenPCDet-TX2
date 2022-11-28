61f3fa47a88420470ac67278f02ffe0c0e5512f5
Daniel-YH-Cai <1155141377@link.cuhk.edu.hk>
2022-11-28 18:13:32 +0800

    Fix vis bug

--- tools/demo.py
+++ tools/demo.py
@@ -99,7 +99,7 @@ def main():
             load_data_to_gpu(data_dict)
             pred_dicts, _ = model.forward(data_dict)
             vis_pickle={}
-            vis_pickle['points']=data_dict['points'][:,1:].cpu().numpy()
+            vis_pickle['points']=data_dict['points'].cpu().numpy()
             vis_pickle['result']=pred_dicts[0]
             for key in ['pred_boxes','pred_scores','pred_labels']:
                 vis_pickle['result'][key]=vis_pickle['result'][key].cpu().numpy()
