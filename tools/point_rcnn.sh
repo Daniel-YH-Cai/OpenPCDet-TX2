c14700e49cc7879799b2bbba08a5ca304a93781d
Daniel-YH-Cai <1155141377@link.cuhk.edu.hk>
2022-11-28 18:18:53 +0800

    change frame

--- tools/point_rcnn.sh
+++ tools/point_rcnn.sh
@@ -1,1 +1,1 @@
-python3 demo.py --cfg_file cfgs/kitti_models/pointrcnn.yaml     --ckpt /home/nvidia/Desktop/motivation-experiment/det3dd/OpenPCDet/ckpt/pointrcnn_7870.pth     --data_path /media/nvidia/Hello_SSD/code/yikang/kitti/files_0018/kitti/training/velodyne_reduced/000001.bin
+python3 demo.py --cfg_file cfgs/kitti_models/pointrcnn.yaml     --ckpt /home/nvidia/Desktop/motivation-experiment/det3dd/OpenPCDet/ckpt/pointrcnn_7870.pth     --data_path /media/nvidia/Hello_SSD/code/yikang/kitti/files_0018/kitti/training/velodyne_reduced/000021.bin
